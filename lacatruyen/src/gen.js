9ddd47244532fc234d06b1b1f45ebbf55b53062c
Nguyen Duan <87711300+DuanNguyen2907@users.noreply.github.com>
2026-02-23 15:05:08 +0700

    Remove lacatruyen icon asset

---
+++ lacatruyen/src/gen.js
@@ -0,0 +1,37 @@
+load('config.js');
+
+function execute(url, page) {
+    var doc = Http.get(page || url).html();
+    var anchors = doc.select('a[href^="/story/"],a[href*="lacatruyen.online/story/"]');
+
+    var data = [];
+    var seen = {};
+    for (var i = 0; i < anchors.size(); i++) {
+        var a = anchors.get(i);
+        var href = a.attr("href");
+        var name = a.text().trim();
+        if (!href || !name) continue;
+
+        var link = absoluteUrl(href).replace(/\/$/, "");
+        if (seen[link]) continue;
+        seen[link] = true;
+
+        var container = a.parent();
+        var cover = "";
+        var description = "";
+        if (container) {
+            cover = container.select("img").first() ? container.select("img").first().attr("src") : "";
+            description = container.select("p").first() ? container.select("p").first().text() : "";
+        }
+
+        data.push({
+            name: name,
+            link: link,
+            cover: absoluteUrl(cover),
+            description: description,
+            host: BASE_URL
+        });
+    }
+
+    return Response.success(data);
+}
---
+++ lacatruyen/src/toc.js
@@ -0,0 +1,47 @@
+load('config.js');
+
+function execute(url) {
+    var fixedUrl = absoluteUrl(url).replace(/\/$/, "");
+    var doc = Http.get(fixedUrl).html();
+    var nextData = parseNextData(doc);
+
+    if (!nextData || !nextData.props || !nextData.props.pageProps) {
+        return Response.error("Không thể đọc danh sách chương");
+    }
+
+    var firstChapterList = nextData.props.pageProps.firstChapter || [];
+    if (!firstChapterList.length) {
+        return Response.success([]);
+    }
+
+    var data = [];
+    var seen = {};
+    var currentSlug = firstChapterList[0].slug;
+    var guard = 0;
+
+    while (currentSlug && guard < 5000) {
+        guard++;
+        var chapterUrl = BASE_URL + "/chapter/" + currentSlug;
+        if (seen[chapterUrl]) break;
+        seen[chapterUrl] = true;
+
+        var chapterDoc = Http.get(chapterUrl).html();
+        var chapterData = parseNextData(chapterDoc);
+        if (!chapterData || !chapterData.props || !chapterData.props.pageProps) break;
+
+        var pageProps = chapterData.props.pageProps;
+        var chapter = pageProps.chapter || {};
+        var chapterTitle = chapter.title || currentSlug;
+
+        data.push({
+            name: chapterTitle,
+            url: chapterUrl,
+            host: BASE_URL
+        });
+
+        var chapterRight = pageProps.chapterRight;
+        currentSlug = chapterRight ? chapterRight.slug : null;
+    }
+
+    return Response.success(data);
+}
